24a7f7ea0d31df2d06f703f610b8f17dd84a5c62
angrybirdpark <thesmileless90@gmail.com>
2022-07-25 14:37:09 +0900

    Add : react-link

--- manage.py
+++ manage.py
@@ -5,7 +5,6 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wesalad.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -15,8 +14,19 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
 
+
+    try:
+        if sys.argv[2] == 'react':
+            project_root = os.getcwd()
+            os.chdir(os.path.join(project_root, "frontend"))
+            os.system("npm run build")
+            os.chdir(project_root)
+            sys.argv.pop(2)
+    except IndexError:
+        execute_from_command_line(sys.argv)
+    else:
+        execute_from_command_line(sys.argv)
 
 if __name__ == '__main__':
     main()
