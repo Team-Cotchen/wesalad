d2e68ad88c928df42b75feed83e6d2594e6cd377
Wonbin Lee <lob3767@gmail.com>
2022-07-31 16:24:19 +0900

    [Modify] Google Signin API Error Fix

--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -24,9 +24,9 @@ class GoogleLoginAPI(APIView):
         app_key = settings.GOOGLE_OAUTH2_CLIENT_ID
         scope   = "https://www.googleapis.com/auth/userinfo.email " + \
                   "https://www.googleapis.com/auth/userinfo.profile"
-        
+
         redirect_uri    = settings.GOOGLE_OAUTH2_REDIRECT_URI
-        google_auth_api = 'https://accounts.google.com/o/oauth2/auth'
+        google_auth_api = 'https://accounts.google.com/o/oauth2/v2/auth'
         
         response = redirect(f'{google_auth_api}?client_id={app_key}&response_type=code&redirect_uri={redirect_uri}&scope={scope}')
         return response
@@ -52,30 +52,30 @@ class GoogleSignInAPI(APIView):
         token['user_id']   = user.id
         token['user_name'] = user.name
                 
-        return Response(token, status=status.HTTP_200_OK)
+        return Response({'token' : token}, status=status.HTTP_200_OK)
 
     def google_get_user_info(self, google_token_api, auth_code):
+
         client_id     = settings.GOOGLE_OAUTH2_CLIENT_ID
         client_secret = settings.GOOGLE_OAUTH2_CLIENT_SECRET
         redirect_uri  = settings.GOOGLE_OAUTH2_REDIRECT_URI
         code          = auth_code
         grant_type    = 'authorization_code'
         state         = 'random_string'
-        
+
         google_token_api += \
             f'?client_id={client_id}&client_secret={client_secret}&code={code}&grant_type={grant_type}&redirect_uri={redirect_uri}&state={state}'
-        
-        google_access_token = requests.post(google_token_api, timeout=2).json()['access_token']
-        
+
+        access_token = requests.post(google_token_api, timeout=3).json()['access_token']
+
         user_info_response = requests.get(
             'https://www.googleapis.com/oauth2/v3/userinfo',
             params = {
-                'access_token': google_access_token
+                'access_token': access_token
             }
         )
         
-        user_info = user_info_response.json()
-        return user_info
+        return user_info_response.json()
 
     def get_or_create(self, user_data):
         if not GoogleSocialAccount.objects.filter(email=user_data['email']).exists():
--- backend/wesalad/settings.py
+++ backend/wesalad/settings.py
@@ -112,7 +112,7 @@ TEMPLATES = [
 ]
 
 #경로 추가
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATICFILES_DIRS = [
 #     '../frontend/build/static'
