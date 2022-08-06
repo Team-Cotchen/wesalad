473661525ff26555f2d65405dcf37355e323bae0
hyodduru <hoje15v@gmail.com>
2022-08-07 07:24:53 +0900

    creation 바로 렌더링 안되는 버그 수정

--- frontend/src/utils/Axios.ts
+++ frontend/src/utils/Axios.ts
@@ -17,11 +17,11 @@ export const AuthVerify = () => {
   const decodedAccess = parseJwt(access);
   const decodedRefresh = parseJwt(refresh);
 
-  if (decodedAccess.exp * 1000 < Date.now()) {
+  if (decodedAccess?.exp * 1000 < Date.now()) {
     return 'Access Token Expired';
   }
 
-  if (decodedRefresh.exp * 1000 < Date.now()) {
+  if (decodedRefresh?.exp * 1000 < Date.now()) {
     return 'Refresh Token Expired';
   }
 
