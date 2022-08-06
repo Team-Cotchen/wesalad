b6308b28ee96298e159a7db571fda68321af6c4f
hyodduru <hoje15v@gmail.com>
2022-08-06 22:18:50 +0900

    버그 수정 : access token 위치 변경 => 생성 페이지에 접근할수 있도록

--- frontend/src/components/PostForm/PostForm.tsx
+++ frontend/src/components/PostForm/PostForm.tsx
@@ -45,7 +45,6 @@ import { useEffect } from 'react';
 
 const { Option } = Select;
 const { Item } = Form;
-const { access } = getToken.getToken();
 
 interface Props {
   mode: 'edit' | 'creation';
@@ -54,6 +53,7 @@ interface Props {
 
 const PostForm: FunctionComponent<Props> = ({ mode, defaultPost }: Props) => {
   const { id } = useParams();
+  const { access } = getToken.getToken();
   const [form] = Form.useForm();
   const navigate = useNavigate();
   const dispatch = useDispatch();
