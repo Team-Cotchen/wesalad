e167f224dfa37ec50b6bb2a60a64dd55c48e59a9
hyodduru <hoje15v@gmail.com>
2022-08-08 20:58:08 +0900

    return config

--- frontend/src/utils/Axios.ts
+++ frontend/src/utils/Axios.ts
@@ -54,6 +54,7 @@ customHttp.interceptors.request.use(
       refresh: getToken().refresh ?? '',
       access: getToken().access ?? '',
     };
+    return config;
   },
   (err) => Promise.reject(err),
 );
