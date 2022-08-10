45e4abc249720f4252a00a2bc144752d9699b9c0
hyodduru <hoje15v@gmail.com>
2022-08-10 23:50:49 +0900

    토큰 삭제

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -11,13 +11,8 @@ const API = {
 };
 
 export const getToken = () => {
-  // const access = localStorage.getItem('accessToken');
-  // const refresh = localStorage.getItem('refreshToken');
-
-  const access =
-    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjYwMTUxNjg1LCJpYXQiOjE2NjAxNDA4ODUsImp0aSI6IjFhYzNlOGMzMGE5MjRkM2NhN2E0MTdhNWU4NWZiYjg4Iiwic3ViIjo5fQ.7nKGi2poJdM03VT7n-ei9l3KkdcWCrMh0Gs5mE2plRc';
-
-  const refresh = `eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTY2MDIyNzI4NSwiaWF0IjoxNjYwMTQwODg1LCJqdGkiOiI0YWQ1MDRhYWViMDE0MDFjODgzZWY3NGQ1YTI3NjBhMCIsInN1YiI6OX0.i91G69zHbvWsFgukA63qeVH7H7Vb0gqr8yFrLVHxXuE`;
+  const access = localStorage.getItem('accessToken');
+  const refresh = localStorage.getItem('refreshToken');
 
   const id = localStorage.getItem('id');
 
