9a508cd52afb43fbad724da9eb0a813db2027360
Geuni <dlrmsgnl620@naver.com>
2022-08-30 00:52:48 +0900

    Delete: lazy 삭제

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,25 @@
-import React, { Suspense, lazy } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Creation from 'pages/Creation/Creation';
-
-const Detail = lazy(() => import('pages/Detail/Detail'));
-const Main = lazy(() => import('pages/Main/index'));
-const Edit = lazy(() => import('pages/Edit/Edit'));
-const GoogleLogin = lazy(() => import('pages/auth/googleLogin'));
-const Setting = lazy(() => import('pages/Setting/Setting'));
-const MyPosts = lazy(() => import('pages/MyPosts/MyPosts'));
+import Detail from 'pages/Detail/Detail';
+import Main from 'pages/Main/index';
+import Edit from 'pages/Edit/Edit';
+import GoogleLogin from 'pages/auth/googleLogin';
+import Setting from 'pages/Setting/Setting';
+import MyPosts from 'pages/MyPosts/MyPosts';
 
 const App = () => {
   return (
     <Router>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="project/:id" element={<Detail />} />
-          <Route path="/creation" element={<Creation />} />
-          <Route path="edit/:id" element={<Edit />} />
-          <Route path="google/callback" element={<GoogleLogin />} />
-          <Route path="/setting" element={<Setting />} />
-          <Route path="myposts" element={<MyPosts />} />
-        </Routes>
-      </Suspense>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="project/:id" element={<Detail />} />
+        <Route path="/creation" element={<Creation />} />
+        <Route path="edit/:id" element={<Edit />} />
+        <Route path="google/callback" element={<GoogleLogin />} />
+        <Route path="/setting" element={<Setting />} />
+        <Route path="myposts" element={<MyPosts />} />
+      </Routes>
     </Router>
   );
 };
