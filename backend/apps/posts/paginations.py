821dcf3ad8a3517878352d8e89b86def4a42f0ec
angrybirdpark <thesmileless90@gmail.com>
2022-09-18 23:48:16 +0900

    Modify : pagination basic setting

--- backend/apps/posts/paginations.py
+++ backend/apps/posts/paginations.py
@@ -2,6 +2,6 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class PostListPagination(PageNumberPagination):
-    page_size             = 20
+    page_size             = 18
     page_size_query_param = 'page_size'
-    max_page_size         = 200
+    max_page_size         = 180
